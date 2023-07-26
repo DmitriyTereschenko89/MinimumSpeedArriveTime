9f258adf72ad072d6e2627d8fe4a5816765b915c
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-26 14:13:50 +0300

    update read.me, add solution

---
+++ MinimumSpeedArriveTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumSpeedArriveTime
+{
+	internal class MinimumSpeedArriveTime
+	{
+		private double ArrivedTime(int[] dist, double speed)
+		{
+			double hours = 0;
+			for (int i = 0; i < dist.Length; ++i)
+			{
+				double time = dist[i] / speed;
+				hours += (i == dist.Length - 1) ? time : Math.Ceiling(time);
+			}
+			return hours;
+		}
+
+		public int MinSpeedOnTime(int[] dist, double hour)
+		{
+			int minSpeed = 1;
+			int minSpeedOnTime = -1;
+			int maxSpeed = 1000000000;
+			while (minSpeed <= maxSpeed)
+			{
+				int middleSpeed = (minSpeed + maxSpeed) / 2;
+				if (ArrivedTime(dist, middleSpeed) <= hour)
+				{
+					maxSpeed = middleSpeed - 1;
+					minSpeedOnTime = middleSpeed;
+				}
+				else
+				{
+					minSpeed = middleSpeed + 1;
+				}
+			}
+			return minSpeedOnTime;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+MinimumSpeedArriveTime.MinimumSpeedArriveTime minimumSpeedArriveTime = new();
+Console.WriteLine(minimumSpeedArriveTime.MinSpeedOnTime(new int[] { 1, 3, 2 }, 6));
+Console.WriteLine(minimumSpeedArriveTime.MinSpeedOnTime(new int[] { 1, 3, 2 }, 2.7));
+Console.WriteLine(minimumSpeedArriveTime.MinSpeedOnTime(new int[] { 1, 3, 2 }, 1.9));
+Console.WriteLine(minimumSpeedArriveTime.MinSpeedOnTime(new int[] { 1, 1, 100000 }, 2.01));
